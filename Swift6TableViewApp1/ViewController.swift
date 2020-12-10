7a855d9ef939d988e98fc7b70799c465d06f0077
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-10 22:19:04 +0900

    UIの作成1

--- Swift6TableViewApp1/ViewController.swift
+++ Swift6TableViewApp1/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var textField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func tap(_ sender: Any) {
+    
+  }
+  
 }
 
